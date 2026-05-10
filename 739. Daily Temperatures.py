7ab4cde7ea990bb147ba6a313e2e2a10a28ea4bd
Priyansh Singh <priyanshsingh855@gmail.com>
2026-05-10 22:16:54 +0530

    Create 739. Daily Temperatures.py

---
+++ 739. Daily Temperatures.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        n = len(temperatures)
+        ans = [0]*n
+        stack = []
+
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev = stack.pop()
+                ans[prev] = i - prev
+            stack.append(i)
+
+        return ans
