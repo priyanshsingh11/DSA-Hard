c7967e827cbbb5a3aaea5319310591224f46b056
Priyansh Singh <priyanshsingh855@gmail.com>
2026-05-16 00:12:52 +0530

    Create 39. Combination Sum.py

---
+++ 39. Combination Sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        result=[]
+
+        def backtrack(index, path, target):
+            if target == 0: result.append(path[:])
+            if target < 0: return 
+
+            for i in range(index, len(candidates)):
+                path.append(candidates[i])
+                backtrack(i, path, target-candidates[i])
+                path.pop()
+
+        backtrack(0, [], target)
+        return result
